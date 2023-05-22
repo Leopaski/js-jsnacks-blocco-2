83ee5c451259b47cf61d852beca278c676193659
Leopaski <leonardo.pascale@gmail.com>
2023-05-22 10:47:07 +0200

    snack-3

---
+++ snack-3/js/myscript.js
@@ -0,0 +1,12 @@
+const numberList=[];
+let i=0;
+sum=0;
+for(i=0;i<10;i++){
+    numberList[i]=parseInt(prompt('Insert a number'))
+    sum=sum+numberList[i]
+}
+const media= sum / numberList.length 
+console.log(sum)
+console.log(sum.toFixed(2))
+
+
