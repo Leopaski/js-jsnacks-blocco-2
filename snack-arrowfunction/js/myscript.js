e3cb1036ee24951979fabc3f11631bbaf7a2f398
Leopaski <leonardo.pascale@gmail.com>
2023-05-30 12:27:50 +0200

    cars

---
+++ snack-arrowfunction/js/myscript.js
@@ -0,0 +1,76 @@
+let cars=[{
+    marca: Fiat, 
+    modello: Panda,
+    alimentazione: gpl,
+},
+{
+    marca: AlfaRomeo, 
+    modello: Giulietta,
+    alimentazione: benzina,
+},
+{
+    marca: Ferrari, 
+    modello: F40,
+    alimentazione: benzina,
+},
+{
+    marca: Lamborghini, 
+    modello: Urus,
+    alimentazione: benzina,
+},
+{
+    marca: Nissan, 
+    modello: Juke,
+    alimentazione: diesel,
+},
+{
+    marca: Volvo, 
+    modello: v60,
+    alimentazione: diesel,
+},
+{
+    marca: Seat, 
+    modello: Arona,
+    alimentazione: metano,
+},
+{
+    marca: Toyota, 
+    modello: Yaris,
+    alimentazione: elettrico,
+},
+{
+    marca: Tesla, 
+    modello: Modelx,
+    alimentazione: elettrico,
+},
+{
+    marca: Fiat, 
+    modello: punto,
+    alimentazione: metano,
+},
+{
+    marca: Porsche, 
+    modello: Carrera,
+    alimentazione: gpl,
+},
+{
+    marca: Dacia, 
+    modello: Logan,
+    alimentazione: gpl,
+},
+{
+    marca: Peugeot, 
+    modello: "206",
+    alimentazione: diesel,
+},
+{
+    marca: Ford, 
+    modello: Fiesta,
+    alimentazione: gpl,
+},
+{
+    marca: Opel, 
+    modello: Corsa,
+    alimentazione: metano,
+}
+]
